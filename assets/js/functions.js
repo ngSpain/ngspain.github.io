e6b5a774a49ecfe8a7b65e3d7d7c60f8e728f52b
JaimeVegaFalcon <jaime@falconsocial.com>
2018-11-01 18:36:43 +0100

    (blog&countdown) countdown with stylying and start blog

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,6 +3,7 @@ $(function() {
 	workBelt();
 	workLoad();
 	clientStuff();
+	setDateCountDown();
 
 	$("header .header-body").fitText(1, { minFontSize: '20px', maxFontSize: '50px' });
 	$(".biglink").fitText(1.5);
@@ -107,6 +108,51 @@ function clientStuff() {
 
   });
 
+}
+
+function setDateCountDown() {
+	var countDownDate = new Date("Jun 23, 2019 09:00").getTime();
+	var oldMonths;
+	var oldDays;
+	var oldMinutes;
+	var oldSeconds;
+
+	var countInterval = setInterval(function () {
+		var now = new Date().getTime();
+		var distance = countDownDate - now;
+
+		if ( distance < 0 ) {
+			document.getElementById("countdown-container").innerHTML = 'Thanks for participating and hope you had an amazing time.';
+			clearInterval(countInterval);
+			return;
+		}
+		
+		var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+		var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 30) / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		if (months !== oldMonths) {
+			document.getElementById("months").innerHTML = months;
+			oldMonths = months;
+		}
+
+		if (days !== oldDays) {
+			document.getElementById("days").innerHTML = days;
+			oldDays = days;
+		}
+
+		if (minutes !== oldMinutes) {
+			document.getElementById("minutes").innerHTML = minutes;
+			oldMinutes = minutes;
+		}
+
+		if (seconds !== oldSeconds) {
+			document.getElementById("seconds").innerHTML = seconds;
+			oldSeconds = seconds;
+		}
+	}, 1000);
 }
 
 
